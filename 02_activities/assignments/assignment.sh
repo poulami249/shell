f552c72aeafb830fb00f1a11f7f66d56b2f72e10
poulami <=>
2024-11-23 10:03:54 -0500

    Completed Assigment1 9th commit

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -39,7 +39,7 @@ cp ./data/raw/event*.log ./data/processed/event_logs/
 rm ./data/raw/*ipaddr*.*
 rm ./data/processed/user_logs/*ipaddr*.*
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
- ls -r ./data/processed/* >> ./data/inventory.txt
+ls -R ./data/processed/* >> ./data/inventory.txt
 
 
 ###########################################
