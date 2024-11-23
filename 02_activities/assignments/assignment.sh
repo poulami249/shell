8bcaa5edc26910f8fc27216d21b5c7a1f73453f7
poulami <=>
2024-11-23 09:28:57 -0500

    Completed Assigment1 7th commit

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -31,15 +31,15 @@ ls ./data/raw
 mkdir ./data/processed
 mkdir ./data/processed/server_logs ./data/processed/user_logs  ./data/processed/event_logs
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-cp .\data\raw\rawdata\server*.log .\data\processed\server_logs\
+cp ./data/raw/rawdata/server*.log ./data/processed/server_logs/
 # 6. Repeat the above step for user logs and event logs
-cp .\data\raw\rawdata\user*.log .\data\processed\user_logs\
-cp .\data\raw\rawdata\event*.log .\data\processed\event_logs\
+cp ./data/raw/rawdata/user*.log ./data/processed/user_logs/
+cp ./data/raw/rawdata/event*.log ./data/processed/event_logs/
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-rm .\data\raw\rawdata\*ipaddr*.*
-rm .\data\processed\user_logs\*ipaddr*.*
+rm ./data/raw/rawdata/*ipaddr*.*
+rm ./data/processed/user_logs/*ipaddr*.*
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
- ls -r .\data\processed\* >> .\data\inventory.txt
+ ls -r ./data/processed/* >> ./data/inventory.txt
 
 
 ###########################################
